dd43c954d8cb4d9982a9e294c9fd10fcb50dcf3c
Pablo Moreno <pablo.a.moreno@gmail.com>
2017-04-14 02:00:23 +0100

    Adds test for verifier of starter KSs

--- src/verifier/test/test_verifier.py
+++ src/verifier/test/test_verifier.py
@@ -1,7 +1,7 @@
 from Bio.Seq import Seq
 from Bio.SeqFeature import SeqFeature, FeatureLocation
 from Bio.SeqRecord import SeqRecord
-from verifier.core import KSDomainVerifier, DH_PS_DomainVerifier
+from verifier.core import KSDomainVerifier, DH_PS_DomainVerifier, KSStarterOnlyVerifiers
 from Clades.core import CladificationAnnotationReader
 import pkg_resources, os, random
 import unittest
@@ -62,6 +62,29 @@ class test_ks_domain_verifier(unittest.TestCase):
         verifier.verify()
         # Verification passing value should be false
         self.assertFalse(dh_domain.qualifiers[KSDomainVerifier.get_qualifier_key()])
+
+    def test_no_KS_before_starter_KS(self):
+        """
+        Checks that a KS which is marked as a starter is marked as not verified if found in the middle
+        :return:
+        :rtype:
+        """
+        resource_package = "Clades.test"  ## Could be any module/package name.
+        # resource_path = os.path.join('annotation_file_1.tab')
+        resource_path = os.path.join('annotation_file_test1.tsv')
+        test_annotation_file = pkg_resources.resource_filename(resource_package, resource_path)
+        clade_annot = CladificationAnnotationReader().get_annotation(test_annotation_file)
+
+        seqobj = SeqRecord(Seq(''.join(
+            [random.choice('ACDEFGHIKLMNPQRSTVWY') for n in xrange(100)])))
+        self.__add_domain_from_to(seqobj, 2, 20, "Clade_2", is_ks=True, region=1)
+        clade_1 = self.__add_domain_from_to(seqobj, 25, 30, "Clade_1", is_ks=True, region=2)
+        self.__add_domain_from_to(seqobj, 40, 50, "Clade_3", is_ks=True, region=3)
+        verifier = KSStarterOnlyVerifiers(seqObj=seqobj, cladification_annotation=clade_annot)
+
+        changes = verifier.verify()
+        self.assertFalse(clade_1.qualifiers[KSStarterOnlyVerifiers.get_qualifier_key()])
+
 
     def __add_domain_from_to(self, seq_record, start, end, name, is_ks=False, region=None):
         qual = {"evalue": "1E-7",
